59fce737259b30b15c3d8ba5876632a4bc6b468b
Kim Maida <kim@kmaida.io>
2017-12-09 20:32:53 -0500

    Add loading states.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styles: [``]
+  styles: []
 })
 export class AppComponent {
 
